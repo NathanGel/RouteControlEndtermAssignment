cb1561f939658594852ec56c776618e475ff23fe
NathanGel <nathan.geleyn@student.hogent.be>
2025-04-01 18:32:44 +0200

    Changes to segment

--- WPFRouteBeheerUI/MainWindow.xaml.cs
+++ WPFRouteBeheerUI/MainWindow.xaml.cs
@@ -16,6 +16,7 @@ namespace WPFRouteBeheerUI {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+            MainFrame.Navigate(new MainPage());
         }
     }
 }
