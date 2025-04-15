367c0ad67465f532b9314f606f01828d15421642
NathanGel <nathan.geleyn@student.hogent.be>
2025-04-15 22:22:11 +0200

    few changes to the file managing proces

--- ConsoleAppImportGegevens/Program.cs
+++ ConsoleAppImportGegevens/Program.cs
@@ -7,41 +7,31 @@ namespace ConsoleAppImportGegevens {
     public class Program {
         static void Main() {
             string connectionString = @"Data Source=NATHAN\SQLExpress;Initial Catalog=NetworkControl;Integrated Security=True;Trust Server Certificate=True";
-            //string pathFacilities = @"C:\Users\natha\Downloads\faciliteiten.txt";
+            string pathFacilities = @"C:\Users\natha\Downloads\faciliteiten.txt";
             string pathNetworkPoints = @"C:\Users\natha\Downloads\network_points.txt";
+            string pathStretches = @"C:\Users\natha\Downloads\stretches.txt";
             Program p = new Program();
             //p.ImportFacilities(pathFacilities, connectionString);
-            p.InitializeNetwork(pathNetworkPoints, connectionString);
+            p.InitializeNetwork(pathFacilities ,pathNetworkPoints, pathStretches, connectionString);
         }
 
-        public void ImportFacilities(string path, string connectionString) {
-            FacilityManager fManager = new FacilityManager(new FaciltyRepository(connectionString));
-            List<Facility> facilities = new();
-            using (StreamReader sr = new(path)) {
-                string line;
-                while ((line = sr.ReadLine()) != null) {
-                    string[] parts = line.Split(',');
-                    facilities.Add(new(parts[1]));
-                }
-                foreach (var facility in facilities) {
-                    fManager.AddFacility(facility);
-                }
-            }
-        }
+        //public void ImportFacilities(string path, string connectionString) {
+        //    FacilityManager fManager = new FacilityManager(new FaciltyRepository(connectionString));
+        //    List<Facility> facilities = new();
+        //    using (StreamReader sr = new(path)) {
+        //        string line;
+        //        while ((line = sr.ReadLine()) != null) {
+        //            string[] parts = line.Split(',');
+        //            facilities.Add(new(parts[1]));
+        //        }
+        //        foreach (var facility in facilities) {
+        //            fManager.AddFacility(facility);
+        //        }
+        //    }
+        //}
 
-        public void InitializeNetwork(string path, string connectionString) {
-            NetworkManager nManager = new NetworkManager(new NetworkRepository(connectionString));
-            List<NetworkPoint> networkPoints = new();
-            using (StreamReader sr = new(path)) {
-                string line;
-                while ((line = sr.ReadLine()) != null) {
-                    string[] parts = line.Split(',');
-                    networkPoints.Add(new(double.Parse(parts[1]), double.Parse(parts[2])));
-                }
-                foreach (var networkPoint in networkPoints) {
-                    nManager.AddNetworkPoint(networkPoint);
-                }
-            }
+        public void InitializeNetwork(string pathFacilities, string pathNetworkPoints, string pathStretches, string connectionString) {
+                        
         }
     }
 }
---
+++ RouteBeheerBL/Managers/FileManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RouteBeheerBL.Interfaces;
+using RouteBeheerBL.Model;
+
+namespace RouteBeheerBL.Managers {
+    public class FileManager {
+        private readonly string _pathFacilities;
+        private readonly string _pathNetworkPoints;
+        private readonly string _pathStretches;
+        private readonly string _connectionString;
+
+        private IReadOnlyList<Facility> _facilities;
+        private IReadOnlyList<NetworkPoint> _networkPoints;
+        private IReadOnlyList<Stretch> _stretches;
+
+
+        private IFacilityRepository _facilityRepository;
+        public FileManager(string pathFacilities, string pathNetworkPoints, string pathStretches, string connectionString) {
+            _pathFacilities = pathFacilities;
+            _pathNetworkPoints = pathNetworkPoints;
+            _pathStretches = pathStretches;
+            _connectionString = connectionString;
+        }
+        
+        public void ReadFacilities() {
+
+        }
+    }
+}
