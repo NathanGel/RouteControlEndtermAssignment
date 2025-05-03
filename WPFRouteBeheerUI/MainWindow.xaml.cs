bcb85b0f075270cde3cd4069bf5c41ddec93d535
NathanGel <nathan.geleyn@student.hogent.be>
2025-05-03 23:03:44 +0200

    added dependencies and the first databaase read to retrieve all points

--- WPFRouteBeheerUI/MainWindow.xaml.cs
+++ WPFRouteBeheerUI/MainWindow.xaml.cs
@@ -8,14 +8,48 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using RouteBeheerBL.Managers;
+using RouteBeheerDL;
+using RouteBeheerBL.Model;
+using System.Collections.ObjectModel;
 
 namespace WPFRouteBeheerUI {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
+        private List<NetworkPoint> points;
+        private List<Segment> segments;
+        private ObservableCollection<Route> routes;
+        private readonly string connectionString = @"Data Source=nathans-laptop\SQLExpress;Initial Catalog=NetworkControlTesting;Integrated Security=True;Trust Server Certificate=True";
+        private NetworkManager nm;
+        private RouteManager rm;
         public MainWindow() {
+            rm = new(new RouteRepository(connectionString));
+            points = new List<NetworkPoint>();
+            segments = new List<Segment>();
+            routes = new ObservableCollection<Route>();
             InitializeComponent();
+            ReadFromDatabase();
+        }
+
+        public void ReadFromDatabase() {
+            nm = new(new NetworkRepository(connectionString));
+            try {
+                List<Segment> segmentsFromDb = new(nm.GetSegments());
+                foreach (var segment in segmentsFromDb) {
+                    segments.Add(segment);
+                }
+
+                List<NetworkPoint> pointsFromDb = new(nm.GetNetworkPoints());
+                foreach (var point in pointsFromDb) {
+                    points.Add(new NetworkPoint(point.Id, point.X, point.Y, point.Facilities));
+                }
+            } catch (ApplicationException ex) { // dit catch-blok vangt elke mogelijke sqlexception die gegooid werden in de repo. De SqlException werd vertaald naar een ApplicationException
+                MessageBox.Show("An error occured while retrieving the network for initialization", ex.Message, MessageBoxButton.OK, MessageBoxImage.Error);
+            } catch (Exception ex) { // dit catch-blok dient als fallback indien er ergens een exception gegooid word die onverwacht is  
+                MessageBox.Show("An unexpected error occured:  " + ex.Message, "System Exception", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
 
         private void Canvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) {
